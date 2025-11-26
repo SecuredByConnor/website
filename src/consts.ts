9c05206341566534b01dd9eef5570f403d941c66
SecuredByConnor <187035534+SecuredByConnor@users.noreply.github.com>
2025-11-26 11:57:24 +0000

    Updated: Site header/footer/home content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Cybersecurity Portfolio';
+export const SITE_DESCRIPTION = 'Welcome to my Cybersecurity Portfolio!';
