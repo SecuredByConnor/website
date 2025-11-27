667d28fd8e0c26cae82f71fa93e5a2e6a011ae23
SecuredByConnor <187035534+SecuredByConnor@users.noreply.github.com>
2025-11-27 11:09:44 +0000

    Updated: Name of site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Cybersecurity Portfolio';
-export const SITE_DESCRIPTION = 'Welcome to my Cybersecurity Portfolio!';
+export const SITE_TITLE = 'SecuredByConnor Write-ups';
+export const SITE_DESCRIPTION = 'Welcome to my writeup site!';
